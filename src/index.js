b49800b62dba1e4464bb0f28aec23923296bbdb4
Liuyixiang <yixiang.liu@thoughtworks.com>
2023-01-01 00:02:50 +0800

    Add babel-loader support tsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { a } from './a.js'
+import { TsxDemo } from './tsx-demo.tsx'
 import { JsxDemo } from './jsx-demo.jsx'
 import { x } from './ts-demo.ts'
 const b = import('./b.js') // 动态引入，也按需加载
 console.log(x)
+console.log(TsxDemo)
 console.log(JsxDemo)
 const hi = () => {
   console.log('hi')
---
+++ src/tsx-demo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const TsxDemo: React.FC = () => {
+  return <div>tsxdemo</div>;
+};
+export { TsxDemo };
