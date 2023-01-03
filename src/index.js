7bb1f4dad928aa68fdeefce71b4e220788f003c9
Liuyixiang <yixiang.liu@thoughtworks.com>
2023-01-03 12:02:42 +0800

    Support scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { a } from 'src/a.js'
 import { TsxDemo } from './tsx-demo.tsx'
 import { JsxDemo } from './jsx-demo.jsx'
 import { x } from './ts-demo.ts'
+import 'src/scss-demo.scss'
 const b = import('./b.js') // 动态引入，也按需加载
 console.log(x)
 console.log(TsxDemo)
